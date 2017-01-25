63f693efa6f87fa3222484772b6f2f51a6bd5212
rohitsukhwal <rohitgit01@gmail.com>
2017-01-25 15:43:48 +0530

    created git repo for vim

---
+++ .vim/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  test.sh
+# 
+#         USAGE:  ./test.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  RohitSharma (), rohit@amagi.com
+#       COMPANY:  Amagi Media labs
+#       VERSION:  1.0
+#       CREATED:  Saturday 03 December 2016 08:56:56  IST IST
+#      REVISION:  ---
+#===============================================================================
+
+
